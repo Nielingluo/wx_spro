5d5bcfd73919d1736dfcea60d1eb750bdf2bedb7
Nielingluo <nielingluo@gmail.com>
2018-01-30 18:21:18 +0800

    tabs-contenet

--- pages/dailynews/dailynews-detail/dailynews-detail.js
+++ pages/dailynews/dailynews-detail/dailynews-detail.js
@@ -1,13 +1,13 @@
 var util = require("../../../utils/util.js");
-var app=getApp();
+var app = getApp();
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    newsdetail:[],
-    currentTabsIndex:0
+    newsdetail: [],
+    currentTabsIndex: 0
   },
 
   /**
@@ -20,12 +20,12 @@ Page({
   },
 
 
-  processNewsData:function(newsdetail){
-     //console.log(newsdetail);
-    var newsdetail={
+  processNewsData: function (newsdetail) {
+    //console.log(newsdetail);
+    var newsdetail = {
       title: newsdetail.title,
       newsImage: newsdetail.image,
-      newsbody:newsdetail.body
+      newsbody: newsdetail.body
     }
     //console.log(newsdetail.newsbody);
     this.setData({
@@ -34,11 +34,11 @@ Page({
   },
 
   //tabs 切换
-  onTabsItemTap:function(event){
+  onTabsItemTap: function (event) {
     var index = event.currentTarget.dataset.index;
     //console.log(index)
     this.setData({
-      currentTabsIndex :index
+      currentTabsIndex: index
     })
 
   },
@@ -50,48 +50,48 @@ Page({
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-  
+
   }
 })
